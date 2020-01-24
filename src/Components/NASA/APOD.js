973aa938c74eef0e27e67edda79f94860e517f5d
Daniel Fieker <danielmfieker@gmail.com>
2020-01-24 10:14:37 -0600

    Finishing Components and Styling

--- src/Components/NASA/APOD.js
+++ src/Components/NASA/APOD.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import SpacePic from "./SpacePic";
-import BibleVerse from "../MyStuff/BibleVerse";
+// import BibleVerse from "../MyStuff/BibleVerse";
 // import PictureData from "./PictureData";
 
 function APOD() {
